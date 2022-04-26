ccb1687bfc4caf7fc258d7d3de9308abe390c98d
IrynaOstapenko <52035358+IrynaOstapenko@users.noreply.github.com>
2022-04-26 09:35:40 +0200

    Changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
  <!-- Creating chart with the help of Chartkick library  -->
-	<h3>Electricity prices in Norway as of {{ formattedDate }}, NOK per kWh</h3>
+	<div v-if="error">{{ error }}</div>
+	<h3 v-if="!error">Electricity prices in Norway as of {{ formattedDate }}, NOK per kWh</h3>
 	<area-chart :data="chartData"></area-chart>
 </template>
 
@@ -8,7 +9,8 @@
 export default {
 	data() {
 		return {
-			chartData: {}
+			chartData: {},
+			error: ''
 		}
 	},
 	computed: {
@@ -26,29 +28,46 @@ export default {
 		async fetchData() {
 			// Fetch data with electricity prices from API
             const url = 'https://playground-norway-power.ffail.win/?zone=NO2&date=2022-01-31&key=2yywtyet';
-            const res = await fetch(url);
-            const result = await res.json();
-            console.log(result);		
-			
-			// Convert the received object into array
-			const arrayOfPrices = Object.entries(result);
-			console.log(arrayOfPrices);
-
-			const arrayOfNeededPrices = arrayOfPrices.map(item => {
-			const container = {};
-
-			// Convert the received array back to object with necessary data
-			container[item[0]] = item[1].NOK_per_kWh;
-			return {
-				[item[0]]: `${(item[1].NOK_per_kWh).toFixed(2)}`
+            const response = await fetch(url);
+			try {
+				await this.handleResponse(response);
+			} catch (error) {
+				this.error = error.message;
 			}
-		})
-		console.log('tot data', arrayOfNeededPrices);
-		
-		const newObject = Object.assign({}, ...arrayOfNeededPrices);
-		this.chartData = newObject;
+		},
 
-		console.log(newObject);
+		async handleResponse(response) {
+			if (response.status >= 200 && response.status < 300)  {
+				const result = await response.json();		
+			
+				// Convert the received object into array
+				const arrayOfPrices = Object.entries(result);
+
+				// Convert the received array back to object with necessary data
+				const arrayOfNeededPrices = arrayOfPrices.map(item => {
+					const container = {};
+					container[item[0]] = item[1].NOK_per_kWh;
+					return {
+						[item[0]]: `${(item[1].NOK_per_kWh).toFixed(2)}`
+					}
+				})
+		
+				//Convert array of objects into one object that can be used in the chart
+				const newObject = Object.assign({}, ...arrayOfNeededPrices);
+				this.chartData = newObject;
+				return true;
+			} else {
+				if (res.status === 404) {
+					throw new Error('Url not found');
+				}
+				if (res.status === 401) {
+					throw new Error('Unauthorized');
+				}
+				if (res.status > 500) {
+					throw new Error('Server error');
+				}
+				throw new Error('Something went wrong');
+			}            
 		}		
 	}
 }
